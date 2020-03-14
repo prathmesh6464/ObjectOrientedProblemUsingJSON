e3ea3462cd87350ae11b06b74b7f9603977d6c2f
prathamesh <prathmesh6464@gmail.com>
2020-03-14 16:18:08 +0530

    MODIFIED : Changed variable names.

--- DeckOfCards/DeckOfCards.java
+++ DeckOfCards/DeckOfCards.java
@@ -6,10 +6,13 @@ import java.util.Random;
 class Cards
 {
 	//ARRAYS
-	String[] rank = {"2", "3", "4", "5", "6", "7", "8", "9", "10","Jack", "Queen", "King", "Ace"};
-	String[] suit = {"Clubs","Diamonds", "Hearts","Spades"};
-	String[][] cards = new String[suit.length][rank.length];
-	String[] player = {"player1","player2","player3","player4"};
+	String[] RANK = {"2", "3", "4", "5", "6", "7", "8", "9", "10","Jack", "Queen", "King", "Ace"};
+	String[] SUIT = {"Clubs","Diamonds", "Hearts","Spades"};
+	String[] PLAYER = {"player1","player2","player3","player4"};
+	
+	
+	//2-D ARRAY CARDS
+	String[][] cards = new String[SUIT.length][RANK.length];
 
 
 	//SHUFFLE METHOD
@@ -17,25 +20,31 @@ class Cards
 	{
 		//RANDOM NUMBER GENERATION
 		Random randomCards = new Random();
-		int row = randomCards.nextInt(suit.length);
-		int column = randomCards.nextInt(rank.length);
+		int row = randomCards.nextInt(SUIT.length);
+		int column = randomCards.nextInt(RANK.length);
 
 
-		//INSETING CARDS AND FETCHING CARDS FROM 2-D ARRAYS
-		for (int suitIterator=0; suitIterator<suit.length; suitIterator++)
+		//INSETING CARDS FROM 2-D ARRAYS
+		for (int suitIterator=0; suitIterator<SUIT.length; suitIterator++)
 		{
-			for(int rankIterator=0; rankIterator<rank.length; rankIterator++)
+			for(int rankIterator=0; rankIterator<RANK.length; rankIterator++)
 			{
-				cards[suitIterator][rankIterator] = suit[suitIterator] + "-" + rank[rankIterator];
+				cards[suitIterator][rankIterator] = SUIT[suitIterator] + "-" + RANK[rankIterator];				
 			}
-			for(int rankIterator=0; rankIterator<rank.length-4; rankIterator++)
+		}
+		
+		
+		//FETCHING CARDS FROM 2-D ARRAYS
+		for (int suitIterator=0; suitIterator<SUIT.length; suitIterator++)
+		{
+			for(int rankIterator=0; rankIterator<RANK.length-4; rankIterator++)
 			{
 				while(cards[row][column] == null)
 				{
-					row = randomCards.nextInt(suit.length);
-					column = randomCards.nextInt(rank.length);
+					row = randomCards.nextInt(SUIT.length);
+					column = randomCards.nextInt(RANK.length);
 				}
-				System.out.println(player[suitIterator]+ " : " + cards[row][column]);
+				System.out.println(PLAYER[suitIterator]+ " : " + cards[row][column]);
 				cards[row][column] = null;
 			}
 			System.out.println();
